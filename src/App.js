8fa63edb018be92e028ec12329be8a8a620a2f85
annoniyous <bouysantiman.y@outlook.com>
2021-01-21 17:03:11 +0100

    Debut header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Components/Header'
+
+import "bootstrap/dist/css/bootstrap.min.css" 
+import "jquery/dist/jquery" 
+import "bootstrap/dist/js/bootstrap.bundle.min"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Header/>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,1 +1,59 @@
 import './Header.css'
+import "bootstrap/dist/css/bootstrap.min.css" 
+import "jquery/dist/jquery" 
+import "bootstrap/dist/js/bootstrap.bundle.min"
+// Mes images
+import logo from '../img/logo.png'
+
+
+function Header() {
+    return (
+      <div className="App">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto col-lg-5 justify-content-center liS">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Actu <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">SEO novice <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Contact <span className="sr-only">(current)</span></a>
+            </li>
+          </ul>
+          <div>
+              <img src={logo} alt="" height="100px"/>
+          </div>
+          <ul className="navbar-nav mr-auto col-lg-5 justify-content-center liS">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Formation <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Conférence <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Audio SEO<span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Video SEO <span className="sr-only">(current)</span></a>
+            </li>
+          </ul>
+          
+        </div>
+      </nav>
+          
+          
+          
+    </div>
+    );
+}
+
+export default Header;
