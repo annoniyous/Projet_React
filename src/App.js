7e9f88d69c49e195bdb80d12ef6bec0be7887a19
annoniyous <bouysantiman.y@outlook.com>
2021-01-22 12:40:49 +0100

    Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-
-import './App.css';
+// Bootrstrap
+import "bootstrap/dist/css/bootstrap.min.css" 
+import "bootstrap/dist/js/bootstrap.bundle.min"
+import './index.css'; 
 import Header from './Components/Header'
 
-import "bootstrap/dist/css/bootstrap.min.css" 
-import "jquery/dist/jquery" 
-import "bootstrap/dist/js/bootstrap.bundle.min"
+// import "jquery/dist/jquery" import "bootstrap/dist/js/bootstrap.bundle.min" 
+// import { faHome } from "@fortawesome/free-solid-svg-icons"; 
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+// import Cards from './Components/Cards'
+
+
+
+import React from "react";
 
 function App() {
   return (
-    <Header/>
+    <div>
+      <Header/>
+
+    </div>
+   
+  
   );
 }
 
